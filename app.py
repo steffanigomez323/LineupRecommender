29c004d14f813e4762441537b51f5e4b0a197c47
kcaluru <krishna_aluru@brown.edu>
2016-02-21 15:57:48 -0500

    setup parent.html for jinja templating

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    error = None
-
-    return "Hello World!"
+    return render_template('index.html')
 
 @app.route('/writeup')
 def writeup():
-    return render_template("writeup.html")
+    return render_template('writeup.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
