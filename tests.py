c73ffd948d854bf3d80a76f1e6ab3a4c5b719e76
kcaluru <krishna_aluru@brown.edu>
2016-02-28 17:59:02 -0500

    fixed imports

--- tests.py
+++ tests.py
@@ -8,6 +8,7 @@ Vann, Steffani, JJ, Chaitu
 Unit Testing
 """
 
+from app import app
 from app import redis_db
 import unittest
 from scrapers import SwishScraper
@@ -16,13 +17,13 @@ from scrapers import SwishScraper
 class FlaskTestCase(unittest.TestCase):
     # test whether index loads
     def test_index_response(self):
-        tester = test_client(self)
+        tester = app.test_client(self)
         response = tester.get('/', content_type='html/text')
         self.assertEqual(response.status_code, 200)
 
     # test whether writeup loads
     def test_writeup_response(self):
-        tester = test_client(self)
+        tester = app.test_client(self)
         response = tester.get('/writeup', content_type='html/text')
         self.assertEqual(response.status_code, 200)
 
