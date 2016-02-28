64fa22c675416633cdb818b714d4d58d83ebf61a
kcaluru <krishna_aluru@brown.edu>
2016-02-28 18:49:03 -0500

    'added ipython'

--- db_helper.py
+++ db_helper.py
@@ -15,15 +15,18 @@ from scrapers import SwishScraper
 class RedisHelper(object):
     # populate the database with all players
     def populate_db(self):
+        # set player_id to 123455
+        redis_db.set('player_id', 123455)
+
         # add all teams
         redis_db.sadd('teams', 'BOS', 'BKN', 'NY', 'PHI',
-                          'TOR', 'CHI', 'CLE', 'DET',
-                          'IND', 'MIL', 'ATL', 'CHA',
-                          'MIA', 'ORL', 'WAS', 'GS',
-                          'LAC', 'LAL', 'PHO', 'SAC',
-                          'DAL', 'HOU', 'MEM', 'NO',
-                          'SA', 'DEN', 'MIN', 'OKC',
-                          'POR', 'UTA')
+                      'TOR', 'CHI', 'CLE', 'DET',
+                      'IND', 'MIL', 'ATL', 'CHA',
+                      'MIA', 'ORL', 'WAS', 'GS',
+                      'LAC', 'LAL', 'PHO', 'SAC',
+                      'DAL', 'HOU', 'MEM', 'NO',
+                      'SA', 'DEN', 'MIN', 'OKC',
+                      'POR', 'UTA')
 
         # add all positions
         redis_db.sadd('positions', 'PG', 'SG', 'SF', 'PF', 'C')
@@ -33,7 +36,9 @@ class RedisHelper(object):
         data = ss.get_players_request()
         players = ss.clean_players_data(data)
         for player in players:
-            player_id = player['player_id']
+            redis_db.incr('player_id')
+            player_id = redis_db.get('player_id')
+            swish_id = player['player_id']
             name = player['player_name']
             team = player['team_abbr']
             position = player['primary_pos_abbr']
@@ -44,6 +49,7 @@ class RedisHelper(object):
             if not redis_db.sismember('positions', position):
                 raise Exception("This player's position is invalid.")
 
-            redis_db.hmset(player_id, {'name': name,
-                                           'team': team,
-                                           'position': position})
+            redis_db.hmset(player_id, {'swish_id': swish_id,
+                                       'name': name,
+                                       'team': team,
+                                       'position': position})
