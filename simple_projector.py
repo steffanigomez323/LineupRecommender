a24c344ff348af93c8a1683f7aad9cec4c579e6a
kcaluru <krishna_aluru@brown.edu>
2016-03-22 02:20:57 -0400

    finished ml stuff

--- simple_projector.py
+++ simple_projector.py
@@ -9,29 +9,57 @@ Simple Optimal Lineup Recommender
 """
 
 
-import random
-import numpy
+import operator
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import r2_score
+from collections import Counter
+import numpy
 
 class SimpleProjector(object):
     def __init__(self, players):
         self.players = players
+        self.avg = lambda x: sum(x) / float(len(x))
 
     def print_all_players(self):
         for player in self.players:
             print player
 
-    """
-    This is a method returns a dictionary from player name to projected score for the next game.
-    """
-    def get_simple_projections(self):
-        avg = lambda x: sum(x) / float(len(x))
-        scores = self.players['Lebron James']
+    def get_models(self, num_games):
         last_n_dict = {}
-        for i in range(1, 6):
-            last_n_dict[i] = [[], []]
-            for j in range(i, len(scores)):
-                last_n_dict[i][0].append(scores[j])
-                last_n_dict[i][1].append(avg(scores[j-i:j]))
-        
+        for player in self.players.iterkeys():
+            last_n_dict[player] = {}
+            for i in range(1, num_games):
+                last_n_dict[player][i] = [[], []]
+                scores = self.players[player]
+                for j in range(i, len(scores)):
+                    last_n_dict[player][i][0].append(scores[j])
+                    last_n_dict[player][i][1].append(self.avg(scores[j-i:j]))
+        return last_n_dict
 
+    def get_best_last_n(self, last_n_dict, num_games):
+        r2_dict = {}
+        for player in last_n_dict.iterkeys():
+            r2_dict[player] = {}
+            for last_n in last_n_dict[player].iterkeys():
+                if len(last_n_dict[player][last_n][0]) > 0:
+                    assert len(last_n_dict[player][last_n][0]) == len(last_n_dict[player][last_n][1])
+                    r2_dict[player][last_n] = r2_score(last_n_dict[player][last_n][0], last_n_dict[player][last_n][1])
+
+        count_14s = 0
+        last_ns = []
+        for player, last_n in r2_dict.iteritems():
+            if len(last_n) == num_games - 1:
+                count_14s += 1
+                last_ns.append(max(last_n.iteritems(), key=operator.itemgetter(1))[0])
+
+        data = Counter(last_ns)
+        return data.most_common(1)[0][0]
+
+    def get_projection(self, scores, averages, last_n):
+        classifier = LogisticRegression()
+        print "#### SCORES"
+        print scores
+        print "#### AVERAGES"
+        print averages
+        classifier.fit(numpy.array([numpy.array([average]) for average in averages]), numpy.array([int(score) for score in scores]))
+        return classifier.predict(self.avg(scores[::-last_n]))
