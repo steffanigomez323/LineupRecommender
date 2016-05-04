02b394be5d01438f716de2c32235ecbde4008459
Vilayvann Ky <vilayvannky@Vilayvanns-MacBook-Pro.local>
2016-05-03 21:06:25 -0400

    store data in redis

--- dummy.py
+++ dummy.py
@@ -1,15 +1,24 @@
 from updater import DailyUpdate
-from projector import SimpleFeatureProjector
+# from projector import SimpleFeatureProjector
 from db_helper import RedisHelper
 
 
-u = DailyUpdate()
-redis = RedisHelper()
-redis.populate_db()
-players = u.nf_playerlookup()
-players_stats = u.get_feature_scores(players)
-sfp = SimpleFeatureProjector(players_stats)
+# u = DailyUpdate()
+# redis = RedisHelper()
+# redis.populate_db()
+# players = u.nf_playerlookup()
+# players_stats = u.get_feature_scores(players)
+# sfp = SimpleFeatureProjector(players_stats)
 
-for p in players.keys():
+# for p in players.keys():
 
-	print p, sfp.get_projection(p)
+# 	print p, sfp.get_projection(p)
+
+from data_collector import NBAStattleShip
+
+ss = NBAStattleShip()
+# ss.get_player_stats_data()
+
+rh = RedisHelper()
+rh.populate_db()
+
