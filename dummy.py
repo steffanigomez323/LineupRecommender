83d7d488d0283267ef4599260defc16cec537d90
kcaluru <krishna_aluru@brown.edu>
2016-05-03 21:45:59 -0400

    done blog_post_3

--- dummy.py
+++ dummy.py
@@ -7,20 +7,5 @@ from data_collector import NBAStattleShip
 
 nss = NBAStattleShip()
 
-
-# u = DailyUpdate()
-# redis = RedisHelper()
-# redis.populate_db()
-# players = u.nf_playerlookup()
-# players_stats = u.get_feature_scores(players)
-# sfp = SimpleFeatureProjector(players_stats)
-
-# for p in players.keys():
-
-# 	print p, sfp.get_projection(p)
-
-# ss.get_player_stats_data()
-
-rh = RedisHelper()
-rh.populate_db()
+print nss.prepare_data_for_projections('nba-lebron-james')
 
