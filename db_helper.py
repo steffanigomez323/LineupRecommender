83a8f306c46a1c31be85ee7727d64f07c8c58ca7
kcaluru <krishna_aluru@brown.edu>
2016-05-05 18:02:02 -0400

    finished nba to stattleship

--- db_helper.py
+++ db_helper.py
@@ -21,12 +21,12 @@ class RedisHelper(object):
         # flush the db
         redis_db.flushall()
 
+        # get player data from stattleship
         stattleship_data = nba_stattleship.get_player_data()
+        # get only the required fields
         stattleship_players = nba_stattleship.get_player_fields(stattleship_data)
 
-        stattleship_id_list = []
-        stattleship_ids = set([])
-
+        # set the player basic information in redis
         for player in stattleship_players:
             name = player["name"]
             weight = player["weight"]
@@ -41,49 +41,67 @@ class RedisHelper(object):
                                             'years_of_experience':
                                             years_of_experience})
 
-            if player["active"]:
-                stattleship_id_list.append(player["slug"])
+        # get nba players' names and ids
+        nba_players = nba_scraper.get_player_data()
+        nba_name_to_id = nba_scraper.get_player_name_id_map(nba_players)
 
-        player_stats = nba_scraper.get_player_stats('2015-16')
-        clean_players_stats = nba_scraper.clean_player_stats2(player_stats)
+        # get stattleship players' names and slugs
+        stattleship_players = nba_stattleship.get_player_data()
+        stattleship_name_to_slug = stattleship.get_player_name_slug_map(stattleship_players)
 
-        nba_names = {}
+        # set all the nba id to stattleship slug maps in redis
+        for nba_name, nba_id in nba_name_to_id.iteritems():
+            stattleship_slug = stattleship_name_to_slug[nba_name]
+            redis_db.set(nba_id, stattleship_slug)
 
-        player_names = nba_scraper.get_players()
-        player_names = nba_scraper.clean_players(player_names)
+        # set all the mismatches manually
+        
 
-        nba_id_list = clean_players_stats.keys()
-        nba_names_list = []
 
-        for id in nba_id_list:
-            name = clean_players_stats[id]['player_name']
-            nba_names[id] = name
-            nba_names_list.append('nba-' + "-".join(name.split(" ")))
 
-            nba_id = player["slug"].split("nba-")[-1]
-            stattleship_ids.add(nba_id.encode('utf-8'))
+        #     if player["active"]:
+        #         stattleship_id_list.append(player["slug"])
 
-        nf_ids = nf_scraper.get_all_player_data()
+        # player_stats = nba_scraper.get_player_stats('2015-16')
+        # clean_players_stats = nba_scraper.clean_player_stats2(player_stats)
 
-        id_not_match = list(nf_ids - stattleship_ids)
-        assert len(id_not_match) == 16
+        # nba_names = {}
+
+        # player_names = nba_scraper.get_players()
+        # player_names = nba_scraper.clean_players(player_names)
+
+        # nba_id_list = clean_players_stats.keys()
+        # nba_names_list = []
+
+        # for id in nba_id_list:
+        #     name = clean_players_stats[id]['player_name']
+        #     nba_names[id] = name
+        #     nba_names_list.append('nba-' + "-".join(name.split(" ")))
+
+        #     nba_id = player["slug"].split("nba-")[-1]
+        #     stattleship_ids.add(nba_id.encode('utf-8'))
+
+        # nf_ids = nf_scraper.get_all_player_data()
+
+        # id_not_match = list(nf_ids - stattleship_ids)
+        # assert len(id_not_match) == 16
         # Cristiano Felicio is shown as 'Chistiano Felicio' in Stattleship
-        redis_db.set('nf-jj-redick', 'nba-j-j-redick')
-        redis_db.set('nf-jose-barea', 'nba-jose-juan-barea')
-        redis_db.set('nf-chris-johnson1', 'nba-chris-johnson')
-        redis_db.set('nf-amare-stoudemire', 'nba-amar-e-stoudemire')
-        redis_db.set('nf-oj-mayo', 'nba-o-j-mayo')
-        redis_db.set('nf-nene-hilario', 'nba-nene')
-        redis_db.set('nf-cj-miles', 'nba-c-j-miles')
-        redis_db.set('nf-jr-smith', 'nba-j-r-smith')
-        redis_db.set('nf-cj-watson', 'nba-c-j-watson')
-        redis_db.set('nf-dj-augustin', 'nba-d-j-augustin')
-        redis_db.set('nf-ishmael-smith', 'nba-ish-smith')
-        redis_db.set('nf-cristiano-felicio', 'nba-chistiano-felicio') 
-        redis_db.set('nf-louis-amundson', 'nba-lou-amundson')
-        redis_db.set('nf-etwaun-moore', 'nba-e-twaun-moore')
-        redis_db.set('nf-jj-hickson', 'nba-j-j-hickson')
-        redis_db.set('nf-roy-devyn-marble', 'nba-devyn-marble')
+        # redis_db.set('nf-jj-redick', 'nba-j-j-redick')
+        # redis_db.set('nf-jose-barea', 'nba-jose-juan-barea')
+        # redis_db.set('nf-chris-johnson1', 'nba-chris-johnson')
+        # redis_db.set('nf-amare-stoudemire', 'nba-amar-e-stoudemire')
+        # redis_db.set('nf-oj-mayo', 'nba-o-j-mayo')
+        # redis_db.set('nf-nene-hilario', 'nba-nene')
+        # redis_db.set('nf-cj-miles', 'nba-c-j-miles')
+        # redis_db.set('nf-jr-smith', 'nba-j-r-smith')
+        # redis_db.set('nf-cj-watson', 'nba-c-j-watson')
+        # redis_db.set('nf-dj-augustin', 'nba-d-j-augustin')
+        # redis_db.set('nf-ishmael-smith', 'nba-ish-smith')
+        # redis_db.set('nf-cristiano-felicio', 'nba-chistiano-felicio') 
+        # redis_db.set('nf-louis-amundson', 'nba-lou-amundson')
+        # redis_db.set('nf-etwaun-moore', 'nba-e-twaun-moore')
+        # redis_db.set('nf-jj-hickson', 'nba-j-j-hickson')
+        # redis_db.set('nf-roy-devyn-marble', 'nba-devyn-marble')
 
         assert len(stattleship_id_list) == 1049
 
--- dummy.py
+++ dummy.py
@@ -7,9 +7,24 @@ from data_collector import NBAStattleShip
 from data_collector import NumberFireScraper
 
 nba = NBAScraper()
-nba_players = nba.clean_players(nba.get_players())
+nba_players = nba.get_player_data()
+nba_name_to_id = nba.get_player_name_id_map(nba_players)
+nba_names = set([])
+
+for nba_name, nba_id in nba_name_to_id.iteritems():
+    nba_names.add(nba_name)
 
 stattleship = NBAStattleShip()
-stattleship_data = stattleship.get_player_data()
-stattleship_players = stattleship.get_player_fields(stattleship_data)
-print stattleship_players
+stattleship_players = stattleship.get_player_data()
+stattleship_name_to_slug = stattleship.get_player_name_slug_map(stattleship_players)
+stattleship_names = set([])
+
+for stattleship_name, stattleship_slug in stattleship_name_to_slug.iteritems():
+    stattleship_names.add(stattleship_name)
+
+for name in nba_names:
+    nba_id = nba_name_to_id[name]
+    stattleship_slug = stattleship_name_to_slug[name]
+    redis_db.set(nba_id, stattleship_slug)
+
+
