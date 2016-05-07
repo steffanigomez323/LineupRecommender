c138ad92cc718deabb05d06f5bdf37791ff6aa9f
kcaluru <krishna_aluru@brown.edu>
2016-05-07 18:01:40 -0400

    added  amap

--- namespace.py
+++ namespace.py
@@ -6,3 +6,35 @@ class Namespace(object):
 
     # redis last updated key
     REDIS_LAST_UPDATED_KEY = "last_updated"
+
+    # team name maps
+    TEAM_MAP_NF_NBA = {"CHI": ,
+                       "CHA": ,
+                       "UTAH" ,
+                       "IND": ,
+                       "NO": ,
+                       "ORL": ,
+                       "MIN": ,
+                       "ATL": ,
+                       "POR": ,
+                       "WSH": ,
+                       "PHX": ,
+                       "MEM": ,
+                       "MIA": ,
+                       "CLE": ,
+                       "GS": ,
+                       "DET": ,
+                       "LAL": ,
+                       "POR": ,
+                       "SAC": ,
+                       "DEN": ,
+                       "TOR": ,
+                       "LAC": ,
+                       "BOS": ,
+                       "BKN": ,
+                       "SA": ,
+                       "NY": ,
+                       "DAL": ,
+                       "OKC": ,
+                       "MIL": ,
+                       "HOU": }
