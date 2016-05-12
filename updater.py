ace906e3d582ded38d6898867b7571991564e313
kcaluru <krishna_aluru@brown.edu>
2016-05-11 23:50:42 -0400

    made some changes

--- updater.py
+++ updater.py
@@ -12,11 +12,42 @@ Update Module
 # from app import namespace
 from projector import DailyProjector
 from db_helper import CSVHelper
+from optimizer import LineupOptimizer
 
 
 class DailyUpdater(object):
-    def update_database(self):
-        last_updated = redis_db.get(namespace.REDIS_LAST_UPDATED_KEY)
+    def get_todays_optimal_lineup(self):
+        print "Storing data in CSVs :)"
+        # let's first get all the data we need and store it in csvs
+        ch = CSVHelper()
+        ch.create_csvs()
+        print "Data successfully stored!"
+
+        print "Preparing data from CSVs :)"
+        # let's prepare out data for projections
+        dp = DailyProjector()
+        dp.prepare_data_for_projections()
+        print "Data is prepared!"
+
+        print "Time to make some projections :)"
+        # let's get the projections based on the prepared data
+        projections = dp.project_fd_score()
+        print "Projections are ready!"
+
+        print "Let's get that golden lineup!!!!"
+        # let's get the optimal lineup based on the projections
+        lo = LineupOptimizer()
+        print lo.optimize()
+        print "Your lineup has been stored in data/optimal_lineup.json. Let's make some cash money ;)"
+        print "This lineup can also be viewed at http://lineup-recommender.herokuapp.com/. Enjoy!"
+
+
+if __name__ == '__main__':
+    du = DailyUpdater()
+    du.get_todays_optimal_lineup()
+
+    # def update_database(self):
+    #     last_updated = redis_db.get(namespace.REDIS_LAST_UPDATED_KEY)
 
     # def get_projections(self):
     #     data = swish_scraper.get_projections()
@@ -137,79 +168,79 @@ class DailyUpdater(object):
     #     for player_id, games_list in player_games.items():
     #         gamelog_id = player_id + "|" + namespace.GAMELOGS
     #         redis_db.lpush(gamelog_id, *games_list)
-
-    def nf_playerlookup(self):
-        player_data = nf_scraper.get_todays_player_data()
-        players = {}
-        for p in player_data:
-            data = redis_db.hgetall("nba-" + p[0])
-            if (len(data) == 0):
-                data.update(redis_db.hgetall("nba-" + redis_db.get(p[0])))
-                players["nba-" + redis_db.get(p[0])] = data
-            else:
-                players["nba-" + p[0]] = data
-        return players
-
-    def get_feature_scores(self, players):
-        players = players.keys()
-        player_stats = {}
-        for player in players:
-
-            data = nba_stattleship.prepare_data_for_projections(player)
-
-            points_tup = data["points"]
-            rebounds_tup = data["rebounds_total"]
-            steals_tup = data["steals"]
-            assists_tup = data["assists"]
-            turnovers_tup = data["turnovers"]
-            blocks_tup = data["blocks"]
-
-            game_time = data["game_time"]
-
-            points_tup_sort = []
-            rebounds_tup_sort = []
-            steals_tup_sort = []
-            assists_tup_sort = []
-            turnovers_tup_sort = []
-            blocks_tup_sort = []
-            for i in range(0, len(points_tup)):
-                time = game_time[i][0:10].split("-")
-                day = datetime.date(int(time[0]), int(time[1]), int(time[2]))
-                points_tup_sort.append((day, points_tup[i]))
-                rebounds_tup_sort.append((day, rebounds_tup[i]))
-                steals_tup_sort.append((day, steals_tup[i]))
-                assists_tup_sort.append((day, assists_tup[i]))
-                turnovers_tup_sort.append((day, turnovers_tup[i]))
-                blocks_tup_sort.append((day, blocks_tup[i]))
-
-            points_tup_sort.sort(key=lambda tup: tup[0])
-            rebounds_tup_sort.sort(key=lambda tup: tup[0])
-            steals_tup_sort.sort(key=lambda tup: tup[0])
-            assists_tup_sort.sort(key=lambda tup: tup[0])
-            turnovers_tup_sort.sort(key=lambda tup: tup[0])
-            blocks_tup_sort.sort(key=lambda tup: tup[0])
-
-
-            if len(points_tup_sort) == 0:
-                continue
-
-            d, points = zip(*points_tup_sort)
-            d, rebounds = zip(*rebounds_tup_sort)
-            d, steals = zip(*steals_tup_sort)
-            d, assists = zip(*assists_tup_sort)
-            d, turnovers = zip(*turnovers_tup_sort)
-            d, blocks = zip(*blocks_tup_sort)
-
-            player_stats[player] = {
-            "points": list(points),
-            "rebounds": list(rebounds),
-            "steals": list(steals),
-            "assists": list(assists),
-            "turnovers": list(turnovers),
-            "blocks": list(blocks),
-            "dates": list(d)}
-        return player_stats
-
+    #
+    # def nf_playerlookup(self):
+    #     player_data = nf_scraper.get_todays_player_data()
+    #     players = {}
+    #     for p in player_data:
+    #         data = redis_db.hgetall("nba-" + p[0])
+    #         if (len(data) == 0):
+    #             data.update(redis_db.hgetall("nba-" + redis_db.get(p[0])))
+    #             players["nba-" + redis_db.get(p[0])] = data
+    #         else:
+    #             players["nba-" + p[0]] = data
+    #     return players
+    #
+    # def get_feature_scores(self, players):
+    #     players = players.keys()
+    #     player_stats = {}
+    #     for player in players:
+    #
+    #         data = nba_stattleship.prepare_data_for_projections(player)
+    #
+    #         points_tup = data["points"]
+    #         rebounds_tup = data["rebounds_total"]
+    #         steals_tup = data["steals"]
+    #         assists_tup = data["assists"]
+    #         turnovers_tup = data["turnovers"]
+    #         blocks_tup = data["blocks"]
+    #
+    #         game_time = data["game_time"]
+    #
+    #         points_tup_sort = []
+    #         rebounds_tup_sort = []
+    #         steals_tup_sort = []
+    #         assists_tup_sort = []
+    #         turnovers_tup_sort = []
+    #         blocks_tup_sort = []
+    #         for i in range(0, len(points_tup)):
+    #             time = game_time[i][0:10].split("-")
+    #             day = datetime.date(int(time[0]), int(time[1]), int(time[2]))
+    #             points_tup_sort.append((day, points_tup[i]))
+    #             rebounds_tup_sort.append((day, rebounds_tup[i]))
+    #             steals_tup_sort.append((day, steals_tup[i]))
+    #             assists_tup_sort.append((day, assists_tup[i]))
+    #             turnovers_tup_sort.append((day, turnovers_tup[i]))
+    #             blocks_tup_sort.append((day, blocks_tup[i]))
+    #
+    #         points_tup_sort.sort(key=lambda tup: tup[0])
+    #         rebounds_tup_sort.sort(key=lambda tup: tup[0])
+    #         steals_tup_sort.sort(key=lambda tup: tup[0])
+    #         assists_tup_sort.sort(key=lambda tup: tup[0])
+    #         turnovers_tup_sort.sort(key=lambda tup: tup[0])
+    #         blocks_tup_sort.sort(key=lambda tup: tup[0])
+    #
+    #
+    #         if len(points_tup_sort) == 0:
+    #             continue
+    #
+    #         d, points = zip(*points_tup_sort)
+    #         d, rebounds = zip(*rebounds_tup_sort)
+    #         d, steals = zip(*steals_tup_sort)
+    #         d, assists = zip(*assists_tup_sort)
+    #         d, turnovers = zip(*turnovers_tup_sort)
+    #         d, blocks = zip(*blocks_tup_sort)
+    #
+    #         player_stats[player] = {
+    #         "points": list(points),
+    #         "rebounds": list(rebounds),
+    #         "steals": list(steals),
+    #         "assists": list(assists),
+    #         "turnovers": list(turnovers),
+    #         "blocks": list(blocks),
+    #         "dates": list(d)}
+    #     return player_stats
+    #
     # def nf_playerlookup(self):
     #     player_data = nf_scraper.get_todays_player_data()
     #     players = {}
@@ -279,16 +310,3 @@ class DailyUpdater(object):
     #         "blocks": list(blocks),
     #         "dates": list(d)}
     #     return player_stats
-
-    def update_projections(self):
-        # let's first get all the data we need and store it in csvs
-        ch = CSVHelper()
-        ch.create_csvs()
-
-        # now let's prepare out data for projections
-        dp = DailyProjector()
-        dp.
-
-if __name__ == '__main__':
-    du = DailyUpdater()
-    du.update_projections()
