bf5869747d60bd36b392eb08f2101bd307b52f48
kcaluru <krishna_aluru@brown.edu>
2016-05-12 01:03:52 -0400

    did some stuff

--- updater.py
+++ updater.py
@@ -17,12 +17,12 @@ from optimizer import LineupOptimizer
 
 class DailyUpdater(object):
     def get_todays_optimal_lineup(self):
-        # # uncomment to also prepare csvs
-        # print "Step 0: Getting data and creating CSVs."
-        # # let's first get all the data we need and store it in csvs
-        # ch = CSVHelper()
-        # ch.create_csvs()
-        # print "---> Data successfully stored!"
+        # uncomment to also prepare csvs
+        print "Step 0: Getting data and creating CSVs."
+        # let's first get all the data we need and store it in csvs
+        ch = CSVHelper()
+        ch.create_csvs()
+        print "---> Data successfully stored!"
 
         print "Step 1: Preparing data from CSVs :)"
         # let's prepare out data for projections
